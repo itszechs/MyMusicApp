ea617ccbe63882540428ddeb7168014897a5bdc0
zechs <vijay.vky2002@gmail.com>
2024-01-15 18:43:13 +0530

    implemented artists list

--- app/src/main/java/zechs/music/ui/artists/ArtistsFragment.kt
+++ app/src/main/java/zechs/music/ui/artists/ArtistsFragment.kt
@@ -1,12 +1,26 @@
 package zechs.music.ui.artists
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.transition.MaterialFadeThrough
 import com.google.android.material.transition.MaterialSharedAxis
-import zechs.music.databinding.FragmentArtistsBinding
+import kotlinx.coroutines.launch
+import zechs.music.databinding.FragmentListBinding
+import zechs.music.ui.artists.adapter.ArtistsAdapter
+import zechs.music.utils.ext.doTransition
+import zechs.music.utils.state.Resource
 
 class ArtistsFragment : Fragment() {
 
@@ -14,8 +28,12 @@ class ArtistsFragment : Fragment() {
         const val TAG = "ArtistsFragment"
     }
 
-    private var _binding: FragmentArtistsBinding? = null
+    private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
+
+    private val artistsAdapter by lazy { ArtistsAdapter(onClick = {}) }
+
+    private val viewModel by activityViewModels<ArtistsViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,13 +47,70 @@ class ArtistsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentArtistsBinding.inflate(inflater, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentArtistsBinding.bind(view)
+        _binding = FragmentListBinding.bind(view)
+
+        setupRecyclerView()
+        songsObserver()
+    }
+
+    private fun songsObserver() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.artists.collect { response ->
+                    when (response) {
+                        is Resource.Success -> response.data?.let {
+                            binding.root.doTransition(MaterialFadeThrough())
+                            artistsAdapter.submitList(it)
+                            isLoading(false)
+                        }
+
+                        is Resource.Error -> {
+                            Log.d(TAG, "Error: ${response.message}")
+                            showSnackBar(response.message)
+                            binding.rvList.isInvisible = true
+                        }
+
+                        is Resource.Loading -> if (!viewModel.hasLoaded) {
+                            isLoading(true)
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun showSnackBar(message: String?) {
+        Snackbar.make(
+            binding.root,
+            message ?: "Something went wrong!",
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun isLoading(hide: Boolean) {
+        binding.apply {
+            loading.isInvisible = !hide
+            rvList.isInvisible = hide
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val linearLayoutManager = LinearLayoutManager(
+            /* context */ context,
+            /* orientation */ RecyclerView.VERTICAL,
+            /* reverseLayout */ false
+        )
+        binding.rvList.apply {
+            adapter = artistsAdapter
+            layoutManager = linearLayoutManager
+        }
     }
 
     override fun onDestroy() {
